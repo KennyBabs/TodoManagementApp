aa80b961fef9287cf9e2f6be553ed941f7f478fb
hayobabson <babalolakehinde.p@gmail.com>
2019-12-08 09:44:20 +0100

    configure application security by extending the spring security configuration adapter

---
+++ src/main/java/com/kennybabs/todomanager/security/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.kennybabs.todomanager.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("admin")
+                .roles("USER", "ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/login", "/h2-console/**").permitAll()
+                .antMatchers("/", "/*todo*/**").access("hasRole('USER')").and()
+                .formLogin();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+}
