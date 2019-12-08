0ff86645c520c6888a843eb90b0f47fc63ba9ae0
hayobabson <babalolakehinde.p@gmail.com>
2019-12-08 12:52:29 +0100

    complete the todo management app

---
+++ src/main/java/com/kennybabs/todomanager/controller/ErrorController.java
@@ -0,0 +1,20 @@
+package com.kennybabs.todomanager.controller;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class ErrorController {
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleException(HttpServletRequest request, Exception ex) {
+        ModelAndView mv = new ModelAndView();
+
+        mv.addObject("exception", ex.getLocalizedMessage());
+        mv.addObject("url", request.getRequestURL());
+
+        mv.setViewName("error");
+        return mv;
+    }
+}
--- src/main/java/com/kennybabs/todomanager/security/SecurityConfiguration.java
+++ src/main/java/com/kennybabs/todomanager/security/SecurityConfiguration.java
@@ -1,24 +1,29 @@
 package com.kennybabs.todomanager.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobalSecurity(AuthenticationManagerBuilder auth)
             throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("admin").password("admin")
+
+                .withUser("admin").password("{noop}admin1")
                 .roles("USER", "ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/login", "/h2-console/**").permitAll()
-                .antMatchers("/", "/*todo*/**").access("hasRole('USER')").and()
+        http.authorizeRequests().antMatchers("/", "/login", "/h2-console/**").permitAll()
+                .antMatchers( "/*todo*/**").access("hasRole('USER')").and()
                 .formLogin();
 
         http.csrf().disable();
